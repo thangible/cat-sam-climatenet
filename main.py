ae74abbd47d4b7f9d088d74df0db3908839af110
Le <phuoc.thang.le@iapt.fraunhofer.de>
2025-03-10 18:17:47 +0100

    auto run

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import time
-os.system("python train.py --config input_config --shot_num 16 --wandb --cat_type cat-a")
+os.system("python train.py --config input_config --shot_num 16 --wandb --cat_type cat-a --run_name 16_a")
 time.sleep(20)
-os.system("python train.py --config input_config --shot_num 16 --wandb --cat_type cat-t")
+os.system("python train.py --config input_config --shot_num 16 --wandb --cat_type cat-t --run_name 16_t")
 time.sleep(20)
-os.system("python train.py --config input_config --shot_num 1 --wandb --cat_type cat-t")
+os.system("python train.py --config input_config --shot_num 1 --wandb --cat_type cat-t --run_name 1_t")
 time.sleep(20)
-os.system("python train.py --config input_config  --wandb --cat_type cat-t")
+os.system("python train.py --config input_config  --wandb --cat_type cat-t --run_name full_t")
 time.sleep(20)
-os.system("python train.py --config input_config  --wandb --cat_type cat-a")
+os.system("python train.py --config input_config  --wandb --cat_type cat-a --run_name full_a")
+time.sleep(20)
+os.system("python train.py --config input_config --shot_num 1 --wandb --cat_type cat-a --run_name 1_a")
