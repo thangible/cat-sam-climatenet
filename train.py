46f3e07855cdffdbfe0fe3915b9cc91d53164aaf
Le <phuoc.thang.le@iapt.fraunhofer.de>
2025-03-11 18:38:53 +0100

    change logging and orders

--- train.py
+++ train.py
@@ -342,6 +342,8 @@ def validate_one_epoch(epoch, val_dataloader, model, iou_eval, device, exp_path,
             val_model.set_infer_img(img=batch['images'])
             masks_pred = val_model.infer(point_coords=batch['point_coords']) if worker_args.dataset == 'm_roads' else val_model.infer(box_coords=batch['box_coords'])
         
+        masks_gt = batch['gt_masks']
+        
         with torch.no_grad():
             _, loss_dict = calculate_losses(masks_pred, masks_gt)
             pred_labels = (torch.sigmoid(masks_pred[0]) > 0.5).float()
@@ -353,7 +355,7 @@ def validate_one_epoch(epoch, val_dataloader, model, iou_eval, device, exp_path,
             recall = intersection / true_labels.sum() if true_labels.sum() != 0 else torch.tensor(0.0)
             f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) != 0 else torch.tensor(0.0)
 
-        masks_gt = batch['gt_masks']
+        
         masks_pred, masks_gt = preprocess_masks(masks_pred, masks_gt)
         
         iou_eval.update(masks_gt, masks_pred, batch['index_name'])
